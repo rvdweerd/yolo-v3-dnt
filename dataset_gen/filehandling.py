b99f7de003802de976b040c2c0b26204b9990cc0
rvdw_vbook <rogerscode@gmail.com>
2021-03-19 00:21:22 +0100

    corrections

--- dataset_gen/filehandling.py
+++ dataset_gen/filehandling.py
@@ -1,22 +1,26 @@
 import os
 
-create=False
-if create:
-    file_obj = open(r"dataset_gen/annotations_.txt","r")
+create_annotations=True
+create_video=True
+#annotation_file="dataset_gen/annotations_243_.txt"
+annotation_file="dataset_gen/annotations_287_done.txt"
+#annotation_file="dataset_gen/annotations_310_.txt"
+if create_annotations:
+    file_obj = open(annotation_file,"r")
     #file_obj.write("test")
     lines=file_obj.readlines()
     while lines[0][0] != lines[1][0]:
         lines[0]=lines[0][1:]
     if not os.path.isdir('datasets_txt'):
             os.mkdir('datasets_txt')
-    listfile=open("datasets_txt/listfile.txt",'w')
+    listfile=open("datasets_txt/listfile.txt",'a')
     fname_old = None
 
     for line in lines:
         tokens1=line.split(",")
         tokens2=line.split()
-        fname_orig=tokens1[0]
-        fname = fname_orig.replace('.png','.txt').replace('.jpg','.txt')
+        fname_orig=tokens1[0].replace('\n',"")#####
+        fname = fname_orig.replace('.png','.txt').replace('.jpg','.txt').replace('\n',"")
         
         
         file_obj=open("datasets_txt/"+fname,"a")
@@ -26,7 +30,9 @@ if create:
             listfile.write(','.join([fname_orig,fname]))
             listfile.write('\n')
         
-        file_obj.write(','.join(tokens1[1:]))
+        str=','.join(tokens1[1:])
+        if str != '':
+            file_obj.write(str)
         
         fname_old=fname_orig
         file_obj.close()
@@ -34,36 +40,46 @@ if create:
 
 import cv2
 import numpy as np
-listfile=open("datasets_txt/listfile.txt",'r')
-lines=listfile.readlines()
 
-fourcc = cv2.VideoWriter_fourcc(*'mp4v') 
-video=cv2.VideoWriter('video.avi', fourcc, 15,(640,480))
+if create_video:
+    listfile=open("datasets_txt/listfile.txt",'r')
+    lines=listfile.readlines()
 
-for line in lines[700:800]:
-    filenames=line.split(',')
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    filenames=lines[0].split(',')
     img=cv2.imread('datasets_img/'+filenames[0])
-    boxfile=open("datasets_txt/"+filenames[1].replace('\n',''))
-    lines=boxfile.readlines()
-    for line in lines:
-        line=line.split(',')
-        cat=float(line[0])
-        xrel=float(line[1])
-        yrel=float(line[2])
-        wrel=float(line[3])
-        hrel=float(line[4])
-        h,w,c=img.shape
-        x0=int((xrel-0.5*wrel)*w)
-        y0=int((yrel-0.5*hrel)*h)
-        x1=int((xrel+0.5*wrel)*w)
-        y1=int((yrel+0.5*hrel)*h)
+    h,w,c=img.shape 
+    print('img shape',w,h,c)
+    video=cv2.VideoWriter('video.avi', fourcc, 15,(640,480))
+    #video=cv2.VideoWriter('video.avi', fourcc, 15,(w,h))
 
-        cv2.rectangle(img,(x0,y0),(x1,y1) ,(0,255,0),2)
-    video.write(img)
-    #cv2.imshow('img',img)
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
+    for line in lines:#[:200]:
+        filenames=line.split(',')
+        img=cv2.imread('datasets_img/'+filenames[0])
+        
+        boxfile=open("datasets_txt/"+filenames[1].replace('\n',''))
+        lines=boxfile.readlines()
+        for line in lines:
+            line=line.split(',')
+            cat=float(line[0])
+            xrel=float(line[1])
+            yrel=float(line[2])
+            wrel=float(line[3])
+            hrel=float(line[4])
+            h,w,c=img.shape
+            #print(img.shape)
+            x0=int((xrel-0.5*wrel)*w)
+            y0=int((yrel-0.5*hrel)*h)
+            x1=int((xrel+0.5*wrel)*w)
+            y1=int((yrel+0.5*hrel)*h)
 
-cv2.destroyAllWindows()
-video.release()
-    
+            cv2.rectangle(img,(x0,y0),(x1,y1) ,(0,255,0),2)
+        img=cv2.copyMakeBorder(img,0,480-h,0,640-w,cv2.BORDER_CONSTANT)
+        video.write(img)
+        #cv2.imshow('img',img)
+        #cv2.waitKey(0)
+        #cv2.destroyAllWindows()
+
+    cv2.destroyAllWindows()
+    video.release()
+        
